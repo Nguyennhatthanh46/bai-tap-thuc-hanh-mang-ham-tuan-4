19afaabe7272d35c2de1a8702ab90544a64385d7
Nguyen Nhat Thanh <nthanh100406@gmail.com>
2024-12-06 11:23:10 +0700

    Xuất mảng chỉ số lẻ

---
+++ Bai 22.cpp
@@ -0,0 +1,40 @@
+Nhập mảng 1 chiều các số nguyên có n phần tử (0<n<100).
+Xuất tất cả các phần tử có chỉ số là số lẻ (chỉ số của mảng bắt đầu từ 0)
+
+Input
+- Nhập số phần tử n.
+- Nhập mảng.
+
+Output
+- Xuất tất cả các phần tử có chỉ số là số lẻ .
+
+VÍ DỤ
+INPUT                 OUTPUT           
+-------------------------------------
+8       	            2 4 6 8
+1 2 3 4 5 6 7 8                   
+
+#include <iostream>
+using namespace std;
+
+void Nhapmang(int a[], int n){
+for(int i = 0; i<n;i++){
+    cin>>a[i];
+}
+}
+
+void Xuatmang(int a[], int n){
+for(int i  = 0; i <n; i++){
+    if(i%2!=0){
+        cout<<a[i]<<" ";
+    }
+}
+}
+int main()
+{
+    int n;
+    cin>>n;
+	int a[n];
+	Nhapmang(a, n);
+	Xuatmang(a, n);
+}
